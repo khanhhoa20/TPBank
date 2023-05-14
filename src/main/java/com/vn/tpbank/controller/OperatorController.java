78b8e43af74329dfe70eb18fa4b95bd76cc9f2af
khanhhoa20 <khanhhoa12t2@gmail.com>
2023-05-14 23:46:24 +0700

    login method

--- src/main/java/com/vn/tpbank/controller/OperatorController.java
+++ src/main/java/com/vn/tpbank/controller/OperatorController.java
@@ -1,13 +1,30 @@
 package com.vn.tpbank.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.vn.tpbank.entity.User;
+import com.vn.tpbank.service.IOperatorService;
 
 @RestController
 @RequestMapping("/tpbank/operator")
 public class OperatorController {
-	
-	// http://localhoat:9090/tpbank/operator/view-customer-list sang
-	
-	//http://localhoat:9090/tpbank/operator/withdraw
+	@Autowired
+	IOperatorService iOperatorService;
+
+	/**
+	 * @URL http://localhoat:9090/tpbank/operator/login
+	 * @param user
+	 * @return message login success or not
+	 * @author khánh hòa
+	 */
+	@PostMapping("login")
+	public String login(@RequestBody User user) {
+		System.out.println("user "+user.getUserName());
+		return iOperatorService.login(user.getUserName(),user.getUserPass());
+		
+	}
 }
--- src/main/java/com/vn/tpbank/service/OperatorServiceImpl.java
+++ src/main/java/com/vn/tpbank/service/OperatorServiceImpl.java
@@ -1,8 +1,23 @@
 package com.vn.tpbank.service;
 
-public class OperatorServiceImpl implements IOperatorService {
-	
-	
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-	
+import com.vn.tpbank.entity.User;
+import com.vn.tpbank.repository.UserRepository;
+
+@Service
+public class OperatorServiceImpl implements IOperatorService {
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	public String login(String username, String pass) {
+		User user = userRepository.findByUserNameAndUserPass(username, pass);
+		if (user != null) {
+			return "Login successfully. Role: " + user.getRole();
+		}
+		return "Wrong username or password!";
+	}
+
 }
