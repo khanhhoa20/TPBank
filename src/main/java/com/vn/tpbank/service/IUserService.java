475ad7bac5c5d633bbf786afda44dea4de088711
khanhhoa20 <khanhhoa12t2@gmail.com>
2023-05-22 13:10:11 +0700

    sth for test

--- src/main/java/com/vn/tpbank/service/IUserService.java
+++ src/main/java/com/vn/tpbank/service/IUserService.java
@@ -1,10 +1,13 @@
 package com.vn.tpbank.service;
 
+import java.util.List;
 import java.util.Optional;
 
+import com.vn.tpbank.entity.Operator;
 import com.vn.tpbank.entity.User;
 
 
 public interface IUserService {
 		public Optional<User> getUserForTest();
+		public List<Operator> getAllOpers();
 }
