ad59f24986fde20d50a7368adbefc2fa95d4c5a0
Đinh Quang Tuấn <quangtuan496@gmail.com>
2023-05-23 10:10:30 +0700

    Update ManagerRepository.java

--- src/main/java/com/vn/tpbank/repository/ManagerRepository.java
+++ src/main/java/com/vn/tpbank/repository/ManagerRepository.java
@@ -1,13 +1,11 @@
 package com.vn.tpbank.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.vn.tpbank.entity.Manager;
 
 @Repository
-public interface ManagerRepository extends JpaRepository<Manager, Long>{
+public interface ManagerRepository extends JpaRepository<Manager, Long> {
+
 }
