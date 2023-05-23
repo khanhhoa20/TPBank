42ab4d106738ff85ff7df2fae89e34ffcfbb5483
dat-vo-63 <trieuvo191@gmail.com>
2023-05-24 00:18:15 +0700

    Create and View Manager

--- src/main/java/com/vn/tpbank/controller/ManagerController.java
+++ src/main/java/com/vn/tpbank/controller/ManagerController.java
@@ -3,6 +3,7 @@ package com.vn.tpbank.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import com.vn.tpbank.service.IManagerService;
 
 @RestController
 @RequestMapping("/tpbank/manager")
+@CrossOrigin
 public class ManagerController {
 	@Autowired
 	IManagerService iManagerService; 
