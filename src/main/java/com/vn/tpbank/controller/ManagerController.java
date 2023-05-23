75081563bef809daf3776dc4cdd8aa22e16caa8f
Tai truong <taitruong1302@gmail.com>
2023-05-23 23:13:05 +0700

    Revert "ok"
    This reverts commit a235bd2c3abf7e3088c191549273b8d125c95a97.

--- src/main/java/com/vn/tpbank/controller/ManagerController.java
+++ src/main/java/com/vn/tpbank/controller/ManagerController.java
@@ -1,8 +1,10 @@
 package com.vn.tpbank.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,11 +12,10 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.DeleteMapping;
 
+import com.vn.tpbank.entity.Manager;
 import com.vn.tpbank.entity.Operator;
 import com.vn.tpbank.entity.User;
-import com.vn.tpbank.entity.Manager;
 import com.vn.tpbank.repository.ManagerRepository;
 import com.vn.tpbank.service.IManagerService;
 
